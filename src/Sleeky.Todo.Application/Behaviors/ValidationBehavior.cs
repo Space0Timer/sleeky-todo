dd0c394a397c2883b01fe11dd28c7ccc3c88d749
Space0Timer <86170651+Space0Timer@users.noreply.github.com>
2026-08-12 11:53:56 +0800

    feat: add mediator validation behavior

---
+++ src/Sleeky.Todo.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,44 @@
+using FluentValidation;
+using FluentValidation.Results;
+
+using MediatR;
+
+namespace Sleeky.Todo.Application.Behaviors;
+
+public sealed class ValidationBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull
+{
+    private readonly IReadOnlyCollection<IValidator<TRequest>> validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        this.validators = validators.ToArray();
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (validators.Count == 0)
+        {
+            return await next(cancellationToken);
+        }
+
+        ValidationContext<TRequest> context = new ValidationContext<TRequest>(request);
+        IEnumerable<Task<ValidationResult>> validationTasks = validators.Select(
+            validator => validator.ValidateAsync(context, cancellationToken));
+        ValidationResult[] validationResults = await Task.WhenAll(validationTasks);
+        ValidationFailure[] failures = validationResults
+            .SelectMany(result => result.Errors)
+            .ToArray();
+
+        if (failures.Length > 0)
+        {
+            throw new ValidationException(failures);
+        }
+
+        return await next(cancellationToken);
+    }
+}
